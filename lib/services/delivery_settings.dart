489c111a6c3749d595ef2d0d2d871cb0f9693826
Sreejita2023 <sensreejita67@gmail.com>
2024-03-02 03:22:22 +0530

    padding added and common widgets removed

--- lib/services/delivery_settings.dart
+++ lib/services/delivery_settings.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import '../common/slider_common.dart';
-import '../common/ui_common.dart';
-
 
 class DeliverySetting extends StatefulWidget {
   const DeliverySetting({super.key});
@@ -20,25 +17,35 @@ class _DeliverySettingState extends State<DeliverySetting> {
   double chargePerKm = 20;
   bool partnerDelivery = true;
   bool enableSelfDelivery = false;
-  int maxOrder=10;
-
+  int maxOrder = 10;
 
   @override
   // initializations
-  void initState(){
+  void initState() {
     super.initState();
+  }
 
+  // checkbox
+  Color getColor(Set<MaterialState> states) {
+    const Set<MaterialState> interactiveStates = <MaterialState>{
+      MaterialState.selected,
+    };
+    if (states.any(interactiveStates.contains)) {
+      return Colors.green;
+    }
+    return Colors.white;
   }
 
   Future<void> addData() async {
     try {
-    //   todo add data to firebase
+      //   todo add data to firebase
     } catch (e) {
-    //   todo add error
+      //   todo add error
     }
   }
-  Future<void> cancelData()async {
-  //    todo cancel data
+
+  Future<void> cancelData() async {
+    //    todo cancel data
   }
 
   @override
@@ -54,100 +61,206 @@ class _DeliverySettingState extends State<DeliverySetting> {
         ),
       ),
       body: SingleChildScrollView(
+        padding: const EdgeInsets.only(left: 16.0,right: 16.0),
         child: Column(
-
           children: [
-            const SizedBox(height: 10,),
-            CheckboxExample(
-                isChecked: partnerDelivery,
-                onChanged: (bool? value) {
-                  setState(() {
-                    partnerDelivery = value!;
-                  });
-                },
-                head: "Tiffinshala Partner Delivery",
-                content: "Partner delivery is enabled"),
-            const SizedBox(height: 10,),
-            SliderCommon(
-              head: "Change maximum orders per hour",
-              content: "",
-              initialValue:maxOrder.toDouble(),
-              division: 15,
-              maxvalue: 15,
-              onChanged: (double value) => {
-                setState(() {
-                  maxOrder = value.toInt();
-                })
-              },
-              keyValue: 5,
+            const SizedBox(
+              height: 10,
             ),
-            const SizedBox(height: 10,),
-            SwitchExample(
-                light: enableSelfDelivery,
-                onChanged: (bool? value) {
-                  setState(() {
-                    enableSelfDelivery = value!;
-                  });
-                },
-                head: "Self Delivery",
-                content: "Select this if you want to manage your own delivery"),
-            const SizedBox(height: 10,),
-            SliderCommon(
-              head: "Base Radius",
-              content: "Base delivery radius",
-              initialValue: baseRadius,
-              division: 10,
-              maxvalue: 5,
-              onChanged: (double value) => {
-                setState(() {
-                  baseRadius = value;
-                })
-              },
-              keyValue: 2,
+            Row(
+              children: [
+                const Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text("Tiffinshala Partner Delivery"),
+                      Text("Partner delivery is enabled"),
+                    ],
+                  ),
+                ),
+                Checkbox(
+                  checkColor: Colors.white,
+                  fillColor: MaterialStateProperty.resolveWith(getColor),
+                  value: partnerDelivery,
+                  onChanged: (bool? value) {
+                    setState(() {
+                      partnerDelivery = value!;
+                    });
+                  },
+                )
+              ],
             ),
-            const SizedBox(height: 10,),
-            SliderCommon(
-              head: "Base Charges",
-              content: "Fixed base delivery charges on every order",
-              initialValue: baseCharge,
-              division: 20,
-              maxvalue: 20,
-              onChanged: (double value) => {
-                setState(() {
-                  baseCharge = value;
-                })
-              },
-              keyValue: 3,
+            const SizedBox(
+              height: 10,
             ),
-            const SizedBox(height: 10,),
-            SliderCommon(
-              head: "Max Radius",
-              content: "Maximum radius where your kitchen shall be listed",
-              initialValue: maxRadius,
-              division: 10,
-              maxvalue: 5,
-              onChanged: (double value) => {
-                setState(() {
-                  maxRadius = value;
-                })
-              },
-              keyValue: 2,
+            // max order
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text("Change maximum orders per hour"),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Expanded(
+                      child: Slider(
+                          value: maxOrder.toDouble(),
+                          max: 15,
+                          divisions: 15,
+                          onChanged: (double value) => {
+                                setState(() {
+                                  maxOrder = value.toInt();
+                                }),
+                              }),
+                    ),
+                    Text(" $maxOrder")
+                  ],
+                )
+              ],
             ),
-            const SizedBox(height: 10,),
-            SliderCommon(
-              head: "Charges Per km",
-              content: "Charges per km after base radius",
-              initialValue: chargePerKm,
-              division: 20,
-              maxvalue: 100,
-              onChanged: (double value) => {
-                setState(() {
-                  chargePerKm = value;
-                })
-              },
-              keyValue: 4,
+            const SizedBox(
+              height: 10,
             ),
-            const SizedBox(height: 10,),
+            // self delivery
+            Row(
+              children: [
+                const Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text("Self Delivery"),
+                      Text(
+                          "Select this if you want to manage your own delivery"),
+                    ],
+                  ),
+                ),
+                Switch(
+                  // This bool value toggles the switch.
+                  value: enableSelfDelivery,
+                  activeColor: Colors.green,
+                  onChanged: (bool? value) {
+                    setState(() {
+                      enableSelfDelivery = value!;
+                    });
+                  },
+                )
+              ],
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+
+            // base radius
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text("Base Radius"),
+                const Text("Base delivery radius"),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Expanded(
+                      child: Slider(
+                          value: baseRadius,
+                          max: 5,
+                          divisions: 10,
+                          onChanged: (double value) => {
+                                setState(() {
+                                  baseRadius = value;
+                                }),
+                              }),
+                    ),
+                    Text(" $baseRadius km")
+                  ],
+                )
+              ],
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            // Base Charges
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text("Base Charges"),
+                const Text("Fixed base delivery charges on every order"),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Expanded(
+                      child: Slider(
+                          value: baseCharge,
+                          max: 20,
+                          divisions: 20,
+                          onChanged: (double value) => {
+                                setState(() {
+                                  baseCharge = value;
+                                }),
+                              }),
+                    ),
+                    Text("₹ $baseCharge")
+                  ],
+                )
+              ],
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            // Max Radius
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text("Max Radius"),
+                const Text("Maximum radius where your kitchen shall be listed"),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Expanded(
+                      child: Slider(
+                          value: maxRadius,
+                          max: 5,
+                          divisions: 10,
+                          onChanged: (double value) => {
+                                setState(() {
+                                  maxRadius = value;
+                                }),
+                              }),
+                    ),
+                    Text("$maxRadius km")
+                  ],
+                )
+              ],
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            // charger per km
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text("Charges Per km"),
+                const Text("Charges per km after base radius"),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Expanded(
+                      child: Slider(
+                          value: chargePerKm,
+                          max: 100,
+                          divisions: 20,
+                          onChanged: (double value) => {
+                                setState(() {
+                                  chargePerKm = value;
+                                }),
+                              }),
+                    ),
+                    Text("₹ $chargePerKm /km")
+                  ],
+                )
+              ],
+            ),
+            const SizedBox(
+              height: 10,
+            ),
             Row(
               children: [
                 Expanded(
@@ -155,21 +268,25 @@ class _DeliverySettingState extends State<DeliverySetting> {
                     onPressed: () {
                       addData();
                     },
-                    child:  const Text("Update"),
+                    child: const Text("Update"),
                   ),
                 ),
-                const SizedBox(width: 4,),
+                const SizedBox(
+                  width: 4,
+                ),
                 Expanded(
                   child: ElevatedButton(
-                      onPressed: () {
-                        cancelData();
-                      },
-                      child:  const Text("Cancel"),
+                    onPressed: () {
+                      cancelData();
+                    },
+                    child: const Text("Cancel"),
                   ),
                 ),
               ],
             ),
-            const SizedBox(height: 10,),
+            const SizedBox(
+              height: 10,
+            ),
           ],
         ),
       ),
